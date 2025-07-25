fd0ecef7e982b084e196d0a9892b4e36361e9f66
khangbkk23 <khang.buitranduycse@hcmut.edu.vn>
2025-07-25 10:11:45 +0700

    First complete code

---
+++ run_facenet.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# run_facenet.sh
+
+# Đường dẫn đến conda
+CONDA_PATH="$HOME/miniconda3"
+
+# Kích hoạt Conda và môi trường
+source "$CONDA_PATH/etc/profile.d/conda.sh"
+conda activate facenet_env
